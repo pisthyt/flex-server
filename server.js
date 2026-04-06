4cd0d238c838c80385bb0226d29e0239884b8d44
pisthyt <marquessalgueirot@gmail.com>
2026-04-06 23:22:21 +0100

    Fix formatting issues in server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Servidor FLEX online');
+  res.send('Servidor FLEX online');
 });
 
 app.listen(PORT, () => {
-    console.log(Servidor a correr na porta ${PORT}`);
+  console.log(Servidor a correr na porta ${PORT}`);
 });
